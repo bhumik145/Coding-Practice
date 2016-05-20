beaece41f27acab1fb9450f660e78f7eb6a4d673
Bhumik Thakkar <bthakkar@ebay.com>
2016-05-19 23:47:56 -0700

    Permutations using recursion

---
+++ src/Permutations.java
@@ -0,0 +1,22 @@
+
+public class Permutations {
+
+	public static void printAllPermutations(String str) {
+		printAllPermutationsRec(str, "");
+	}
+
+	private static void printAllPermutationsRec(String str, String prefix) {
+		if (str.isEmpty()) {
+			System.out.println(prefix);
+		} else {
+			for (int index = 0; index < str.length(); index++) {
+				String remaining = str.substring(0, index) + str.substring(index + 1);
+				printAllPermutationsRec(remaining, prefix + str.charAt(index));
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		printAllPermutations("abcd");
+	}
+}
