6284a5912daf64baf225e1a22811abb480c01ecd
Bhumik Thakkar <bthakkar@ebay.com>
2016-06-15 19:24:14 -0700

    Check if two strings are 1 edit distance away

---
+++ src/arraysandstrings/EditDistance.java
@@ -0,0 +1,67 @@
+package arraysandstrings;
+
+import java.util.Map;
+import java.util.Map.Entry;
+
+import util.StringUtils;
+
+public class EditDistance {
+
+	public static boolean isEditDistanceLessThan2Smart(String first, String second) {
+
+		int len1 = first.length();
+		int len2 = second.length();
+
+		if (len1 == len2) {
+			// check for one replace
+			if (getNumberOfReplacedRequired(first, second) > 1) {
+				return false;
+			} else {
+				return true;
+			}
+		} else if (Math.abs(len1 - len2) == 1) {
+			// Check for insert/remove
+			boolean isOneInsertEnough = false;
+			if (len1 < len2) {
+				isOneInsertEnough = isOneInsertEnough(first, second);
+			} else {
+				isOneInsertEnough = isOneInsertEnough(second, first);
+			}
+			return isOneInsertEnough;
+		} else {
+			return false;
+		}
+	}
+
+	public static int getNumberOfReplacedRequired(String first, String second) {
+		int totalReplaces = 0;
+		for (int index = 0; index < first.length(); index++) {
+			if (first.charAt(index) != second.charAt(index)) {
+				totalReplaces++;
+			}
+		}
+		return totalReplaces;
+	}
+
+	public static boolean isOneInsertEnough(String small, String big) {
+		int numberOfInserts = 0;
+
+		for (int index1 = 0, index2 = 0; index1 < small.length() && index2 < big.length();) {
+			if (small.charAt(index1) != big.charAt(index2)) {
+				numberOfInserts++;
+				index2++;
+			} else {
+				index1++;
+				index2++;
+			}
+		}
+		return numberOfInserts <= 1;
+	}
+
+	public static void main(String[] args) {
+		String str1 = "pale";
+		String str2 = "cale";
+		System.out.println(isEditDistanceLessThan2Smart(str1, str2));
+	}
+
+}
