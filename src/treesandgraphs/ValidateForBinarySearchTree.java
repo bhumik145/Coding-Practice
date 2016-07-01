47e2b7a30e5c50fadc8904cec65b9db28127a544
Bhumik Thakkar <bthakkar@ebay.com>
2016-06-30 23:23:25 -0700

    valid binary search tree

--- src/treesandgraphs/ValidateForBinarySearchTree.java
+++ src/treesandgraphs/ValidateForBinarySearchTree.java
@@ -2,48 +2,33 @@ package treesandgraphs;
 
 public class ValidateForBinarySearchTree {
 
-	public static <T> boolean isValidBinarySearchTree(TreeNode<T> node) {
+	public static boolean isValidBinarySearchTree(TreeNode<Integer> node, int min, int max) {
+
 		if (null == node) {
 			return true;
 		} else {
-			boolean isLeftValid = isValidBinarySearchTree(node.left);
-			boolean isRightValid = isValidBinarySearchTree(node.right);
-			if (isLeftValid && isRightValid && isGivenNodeValidForBinarySearchTree(node)) {
-				return true;
+			if (node.data >= min && node.data < max) {
+				return isValidBinarySearchTree(node.left, min, node.data)
+						&& isValidBinarySearchTree(node.right, node.data, max);
 			} else {
 				return false;
 			}
 		}
-	}
 
-	public static <T> boolean isGivenNodeValidForBinarySearchTree(TreeNode<T> node) {
-		boolean isValid = true;
-		if (null != node.left) {
-			// node should be greater than or equal to it's left
-			if (node.compareTo(node.left) < 0) {
-				isValid = false;
-			}
-		}
-		if (null != node.right) {
-			// node should be less than it's right
-			if (node.compareTo(node.right) >= 0) {
-				isValid = false;
-			}
-		}
-		return isValid;
 	}
 
 	public static void main(String[] args) {
 		Tree<Integer> tree = new Tree<>();
+
 		tree.insert(50).insert(40).insert(100).insert(45).insert(60).insert(110).insert(10).insert(20).insert(24);
 		TreeNode<Integer> newNode = new TreeNode<>(200);
 		tree.insert(newNode);
-		
-		TreeNode<Integer> unbalancedNode = new TreeNode<>(300);
+
+		TreeNode<Integer> unbalancedNode = new TreeNode<>(22);
 		newNode.left = unbalancedNode;
-		
+
 		Tree.printLevelByLevel(tree.root);
-		System.out.println("isValid binary search tree: "+isValidBinarySearchTree(tree.root));
+		System.out.println("isValid binary search tree: " + isValidBinarySearchTree(tree.root, Integer.MIN_VALUE, Integer.MAX_VALUE));
 
 	}
 
